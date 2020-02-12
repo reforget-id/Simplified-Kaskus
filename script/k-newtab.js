8daeeffc76791270c346557b67cc8fb6b7f62efa
reforget-id <58993868+reforget-id@users.noreply.github.com>
2020-02-12 15:17:55 +0700

    Update k-newtab.js

--- script/k-newtab.js
+++ script/k-newtab.js
@@ -1,9 +1,13 @@
 // ==UserScript==
 // @name        Kaskus : Open in New Tab
-// @version     1.0.0
-// @namespace   https://github.com/reforget-id/Simplified-Kaskus-Mobile-Webview
+// @version     1.1.0
+// @icon        https://www.google.com/s2/favicons?domain=m.kaskus.co.id
+// @namespace   ffsuperteam
+// @homepage    https://github.com/reforget-id/Simplified-Kaskus-Mobile-Webview
 // @description Buka Last Post, First New Post, Hot Thread di Tab Baru
 // @include     https://m.kaskus.co.id/*
+// @downloadURL https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-newtab.js
+// @updateURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-newtab.js
 // @grant		none
 // ==/UserScript==
 
@@ -24,6 +28,11 @@ function getId(element) {
 
 document.addEventListener("click", function(e){
 	var anchor = getAnchor(e.target);
+
+    if (!anchor || anchor.target || anchor.protocol == "javascript:" || e.isTrusted === false || !anchor.offsetParent || (e.isTrusted == null && !e.detail)) {
+		return;
+	}
+
     var atthref = anchor.getAttribute("href");
     var keyhref = /.*(\/lastpost\/|\?goto=newpost|&med=hot_thread).*/gm;
     var mhref = atthref.match(keyhref);
