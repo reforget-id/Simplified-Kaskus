7ada9041aa25d00c79fc1e223ff2206ee9cf07ce
reforget-id <58993868+reforget-id@users.noreply.github.com>
2020-04-05 03:00:15 +0700

    Create k-allpost.js

---
+++ script/k-allpost.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name          Kaskus : Show All Post
+// @version       1.0.0
+// @namespace     k-allpost
+// @author        ffsuperteam
+// @icon          https://www.google.com/s2/favicons?domain=m.kaskus.co.id
+// @homepage      https://github.com/reforget-id/Simplified-Kaskus
+// @description   Otomatis redirect show all post
+// @include       https://*.kaskus.co.id/show_post/*
+// @downloadURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-allpost.js
+// @updateURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-allpost.js
+// ==/UserScript==
+
+
+function showPost(){
+		var allpost = document.getElementsByClassName("Mend(15px)");
+		for (var i = 0; i < allpost.length ; i++) {
+				allpost[i].click();
+		}
+		
+}
+
+showPost();
