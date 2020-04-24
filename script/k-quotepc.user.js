3b25c7c1f77e11380c6b6ffe8ec70c61a5259a65
reforget-id <58993868+reforget-id@users.noreply.github.com>
2020-04-25 01:23:15 +0700

    Update k-quotepc.user.js

--- script/k-quotepc.user.js
+++ script/k-quotepc.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Kaskus : Insert Quote Button for PC
-// @version       1.1.0
+// @version       1.5.0
 // @namespace     k-quotepc
 // @author        ffsuperteam
 // @icon          https://www.google.com/s2/favicons?domain=m.kaskus.co.id
@@ -11,22 +11,20 @@
 // @include       https://www.kaskus.co.id/show_post/*
 // @include       https://www.kaskus.co.id/lastpost/*
 // @include       https://www.kaskus.co.id/post_reply/*
-// @downloadURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-quotepc.user.js
-// @updateURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-quotepc.user.js
+// @downloadURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-quotepc.js
+// @updateURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-quotepc.js
 // @grant		      GM_addStyle
 // @grant		      GM_setValue
 // @grant		      GM_getValue
 // @grant		      GM_deleteValue
-// @run-at        document-idle
+// @run-at        document-end
 
 // ==/UserScript==
 
 
 GM_addStyle(`
 .single-quote {
-margin-right: 18px !important;
-padding-right: 10px !important;
-margin-top: 5px !important;
+padding-right: 3px !important;
 font-size: 15px !important;
 }
 `);
@@ -56,19 +54,64 @@ function getElementsByIdStartsWith(selectorTag, prefix) {
 }
 
 function singleQuote(){
-    var list = document.getElementsByClassName("Fx(flexOne) D(f) Ai(c) vote-wrapper");
+    var list = document.getElementsByClassName("jsButtonMultiquote buttonMultiquote");
     var thread = document.getElementById("thread_id");
     var post = getElementsByIdStartsWith("div", "post");
 
     for (var i = 0; i < list.length ; i++) {
-        var NewElement = document.createElement('a');
+        var NewElement1 = document.createElement('a');
+			  var NewElement2 = document.createElement('i');
+			  var NewElement3 = document.createElement('span');
         var threadid = thread.getAttribute("value");
         var postid = post[i].getAttribute("id");
-
-        NewElement.setAttribute("class", "single-quote fas C(c-secondary) fa-comment Mend(2px)");
-        NewElement.href = "/post_reply/" + threadid + "/?post=" + postid.match(/[^post].*/g);
-        NewElement.appendAfter(list[i]);        
+				
+				NewElement1.setAttribute("class", "D(ib) Td(n):h Fz(16px) Mend(15px) Px(8px) Py(3px) Bdrs(8px) buttonMultiquote");
+			  NewElement1.href = "/post_reply/" + threadid + "/?post=" + postid.match(/[^post].*/g);
+			  NewElement1.appendBefore(list[i]);  
+			  NewElement1.appendChild(NewElement2);
+			  NewElement1.appendChild(NewElement3);
+        NewElement2.setAttribute("class", "single-quote fas C(c-secondary) fa-comment Mend(2px)");
+			  NewElement3.setAttribute("class", "C(c-secondary) Fz(12px)");
+			  NewElement3.innerHTML = "Single Quote";
     }
+}
+
+
+function nestedSingleQuote(){
+    var list = document.getElementsByClassName("jsButtonReply buttonReply");
+    var thread = document.getElementById("thread_id");
+    var post = getElementsByIdStartsWith("div", "post");
+	  var p1 = document.getElementsByClassName("Fx(flexOne) D(f) Ai(c) vote-wrapper");
+
+    for (var i = 0; i < list.length ; i++) {
+        var NewElement1 = document.createElement('a');
+			  var NewElement2 = document.createElement('i');
+			  var NewElement3 = document.createElement('span');
+        var threadid = thread.getAttribute("value");
+        var postid = post[i].getAttribute("id");	  
+			
+				if(list[i].className == 'jsButtonReply buttonReply'){
+						NewElement1.setAttribute("class", "buttonMultiquote Mend(15px) Px(8px)");
+						NewElement1.href = "/post_reply/" + threadid + "/?post=" + postid.match(/[^post].*/g);
+						NewElement1.appendBefore(list[i]);  
+						NewElement1.appendChild(NewElement2);
+						NewElement1.appendChild(NewElement3);
+						NewElement2.setAttribute("class", "single-quote fas C(c-secondary) fa-comment Mend(2px)");
+						NewElement3.setAttribute("class", "C(c-secondary) Fz(12px)");
+						NewElement3.innerHTML = "Single Quote";
+				}
+    }
+}
+
+
+function replaceKutip(){
+		var list = document.getElementsByTagName("span");
+		for (var i = 0; i < list.length ; i++) {
+				if(list[i].innerHTML == 'Kutip'){
+						list[i].innerHTML = list[i].innerHTML.replace("Kutip","Multi Quote");		
+				}
+		}
+	
 }
 
 
@@ -88,13 +131,15 @@ function getText(){
 		GM_deleteValue("quote");	
 }
 
+replaceKutip()
+singleQuote();
 
 window.onload=function(){
     var reply = document.getElementsByClassName("Fx(flexZero) jsShowNestedTrigger Cur(p)");
-    for (var i = 0; i < reply.length ; i++) {
-        reply[i].click();
+    for (var j = 0; j < reply.length ; j++) {
+        reply[j].click();
     }
-    setTimeout(singleQuote, 2500);
+    setTimeout(nestedSingleQuote, 2500);
 }
 
 getText();
