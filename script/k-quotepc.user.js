f20f653118051a2d15de9ed5714daf703a739507
reforget-id <58993868+reforget-id@users.noreply.github.com>
2020-04-25 11:37:57 +0700

    Update k-quotepc.user.js

--- script/k-quotepc.user.js
+++ script/k-quotepc.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
-// @name          Kaskus : Insert Quote Button for PC
-// @version       1.5.1
-// @namespace     k-quotepc
+// @name          Kaskus : Insert Quote Button
+// @version       2.1.2
+// @namespace     k-quote
 // @author        ffsuperteam
 // @icon          https://www.google.com/s2/favicons?domain=m.kaskus.co.id
 // @homepage      https://github.com/reforget-id/Simplified-Kaskus
-// @description   Tambah tombol quote di nested reply di PC
-// @include       https://www.kaskus.co.id/thread/*
-// @include       https://www.kaskus.co.id/post/*
-// @include       https://www.kaskus.co.id/show_post/*
-// @include       https://www.kaskus.co.id/lastpost/*
-// @include       https://www.kaskus.co.id/post_reply/*
-// @downloadURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-quotepc.user.js
-// @updateURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-quotepc.user.js
+// @description   Tambah tombol quote di nested reply
+// @include       https://m.kaskus.co.id/thread/*
+// @include       https://m.kaskus.co.id/post/*
+// @include       https://m.kaskus.co.id/show_post/*
+// @include       https://m.kaskus.co.id/lastpost/*
+// @include       https://m.kaskus.co.id/post_reply/*
+// @downloadURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-quote.user.js
+// @updateURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/k-quote.user.js
 // @grant		      GM_addStyle
 // @grant		      GM_setValue
 // @grant		      GM_getValue
@@ -23,23 +23,24 @@
 
 
 GM_addStyle(`
+.quote_btn {
+margin-right: 18px !important;
+padding-right: 10px !important;
+font-size: 16px !important;
+color: var(--c-secondary);
+}
 .single-quote {
-padding-right: 3px !important;
-font-size: 15px !important;
+margin-right: 18px !important;
+padding-right: 10px !important;
+color: var(--c-secondary);
 }
 `);
 
 
 var link = document.URL;
 
-/* Adds Element BEFORE NeighborElement */
-Element.prototype.appendBefore = function(element) {
-  element.parentNode.insertBefore(this, element);
-}, false;
-
-/* Adds Element AFTER NeighborElement */
-Element.prototype.appendAfter = function(element) {
-  element.parentNode.insertBefore(this, element.nextSibling);
+Element.prototype.appendBefore = function (element) {
+		element.parentNode.insertBefore(this, element);
 }, false;
 
 function getElementsByIdStartsWith(selectorTag, prefix) {
@@ -53,100 +54,71 @@ function getElementsByIdStartsWith(selectorTag, prefix) {
         return items;
 }
 
-function singleQuote(){
-    var list = document.getElementsByClassName("jsButtonMultiquote buttonMultiquote");
-		var listreply = document.getElementsByClassName("D(ib) Td(n):h Fz(16px) jsButtonReply buttonReply");
+function quoteNested(){
+    var list = document.getElementsByClassName("D(f) Jc(fs) Ai(c) Mstart(10px) C(#b3b3b3) reply-btn");
     var thread = document.getElementById("thread_id");
-    var post = getElementsByIdStartsWith("div", "post");
-
-    for (var i = 0; i < listreply.length ; i++) {
-        var NewElement1 = document.createElement('a');
-			  var NewElement2 = document.createElement('i');
-			  var NewElement3 = document.createElement('span');
-        var threadid = thread.getAttribute("value");
-        var postid = post[i].getAttribute("id");
-				
-				NewElement1.setAttribute("class", "D(ib) Td(n):h Fz(16px) Mend(15px) Px(8px) Py(3px) Bdrs(8px) buttonMultiquote");
-			  NewElement1.href = "/post_reply/" + threadid + "/?post=" + postid.match(/[^post].*/g);			  
-			  NewElement1.appendChild(NewElement2);
-			  NewElement1.appendChild(NewElement3);
-        NewElement2.setAttribute("class", "single-quote fas C(c-secondary) fa-comment Mend(2px)");
-			  NewElement3.setAttribute("class", "C(c-secondary) Fz(12px)");
-			  NewElement3.innerHTML = "Single Quote";
-				
-				if(window.location.href.match(/^.*www\.kaskus\.co\.id\/show_post\/*./g)){
-						NewElement1.appendBefore(listreply[i]);  
-				}
-				else{
-						NewElement1.appendBefore(list[i]);  
-				}
-    }
-}
-
-
-function nestedSingleQuote(){
-    var list = document.getElementsByClassName("jsButtonReply buttonReply");
-    var thread = document.getElementById("thread_id");
-    var post = getElementsByIdStartsWith("div", "post");
-	  var p1 = document.getElementsByClassName("Fx(flexOne) D(f) Ai(c) vote-wrapper");
+    var post = getElementsByIdStartsWith("div", "nestedbit-");
 
     for (var i = 0; i < list.length ; i++) {
-        var NewElement1 = document.createElement('a');
-			  var NewElement2 = document.createElement('i');
-			  var NewElement3 = document.createElement('span');
+        var NewElement = document.createElement('a');
         var threadid = thread.getAttribute("value");
-        var postid = post[i].getAttribute("id");	  
-			
-				if(list[i].className == 'jsButtonReply buttonReply'){
-						NewElement1.setAttribute("class", "buttonMultiquote Mend(15px) Px(8px)");
-						NewElement1.href = "/post_reply/" + threadid + "/?post=" + postid.match(/[^post].*/g);
-						NewElement1.appendBefore(list[i]);  
-						NewElement1.appendChild(NewElement2);
-						NewElement1.appendChild(NewElement3);
-						NewElement2.setAttribute("class", "single-quote fas C(c-secondary) fa-comment Mend(2px)");
-						NewElement3.setAttribute("class", "C(c-secondary) Fz(12px)");
-						NewElement3.innerHTML = "Single Quote";
+        var postid = post[i].getAttribute("id");
+
+        NewElement.setAttribute("class", "quote_btn fas fa-comment Fz(20px)");
+        NewElement.href = "/post_reply/" + threadid + "/?post=" + postid.match(/[^nestedbit-].*/g);
+        NewElement.appendBefore(list[i]);
+        
+    }
+}
+
+function singleQuote(){
+		var list = document.getElementsByClassName("quote-btn D(f) Jc(fs) Ai(c) Mend(10px) Cur(p) is-multiquoted_Bgc(c-orange-night) Px(8px) Py(3px) Bdrs(8px) is-multiquoted_C(c-white) is-multiquoted_Fw(500)");  
+	  var listreply = document.getElementsByClassName("D(f) Jc(fs) Ai(c) Cur(p) Mstart(10px) C(c-secondary) nightmode_C(c-secondary-night) reply-btn");  
+    var thread = document.getElementById("thread_id");
+    var post = getElementsByIdStartsWith("div", "cendol");
+
+    for (var i = 0; i < listreply.length ; i++) {
+        var NewElement = document.createElement('a');
+        var threadid = thread.getAttribute("value");
+        var postid = post[i].getAttribute("id");
+
+        NewElement.setAttribute("class", "single-quote fas fa-comment Fz(20px)");
+        NewElement.href = "/post_reply/" + threadid + "/?post=" + postid.match(/[^cendol].*/g);
+			  
+				if(window.location.href.match(/^.*m\.kaskus\.co\.id\/show_post\/*./g)){
+						NewElement.appendBefore(listreply[i]);  
+				}
+				else{
+						NewElement.appendBefore(list[i]);  
 				}
     }
 }
 
-
-function replaceKutip(){
-		var list = document.getElementsByTagName("span");
-		for (var i = 0; i < list.length ; i++) {
-				if(list[i].innerHTML == 'Kutip'){
-						list[i].innerHTML = list[i].innerHTML.replace("Kutip","Multi Quote");		
-				}
-		}
-	
-}
-
-
 function getText(){
-    var elem = document.getElementById("reply-messsage").value;
+    var elem = document.getElementById("jsCreateThread").value;
     var link = document.URL;
-    if (link == link.match(/^.*\/\?post=.*/g)){		
+    if (link == link.match(/^.*\/\?post=.*/g)){			
         GM_setValue("quote", elem);
         link = link.match(/^.*\//g);
         window.location.href = link;
-    }
-
-    if (link == link.match(/^.*\/$/g)){
-        document.getElementById("reply-messsage").value = GM_getValue("quote");
-    }
+		}
 	
-		GM_deleteValue("quote");	
+     if (link == link.match(/^.*\/$/g)){
+      	document.getElementById("jsCreateThread").value = GM_getValue("quote");
+    }	
+	
+     GM_deleteValue("quote");
 }
 
-replaceKutip()
+
 singleQuote();
 
 window.onload=function(){
-    var reply = document.getElementsByClassName("Fx(flexZero) jsShowNestedTrigger Cur(p)");
-    for (var j = 0; j < reply.length ; j++) {
-        reply[j].click();
+    var reply = document.getElementsByClassName("Fx(flex0Auto) jsShowNestedTrigger");
+    for (var i = 0; i < reply.length ; i++) {
+        reply[i].click();
     }
-    setTimeout(nestedSingleQuote, 2000);
+    setTimeout(quoteNested, 2500);
 }
 
 getText();
