217d62cd937617c4250527461a4179cdf2c74069
fahriza.fahmi99@gmail.com <fahriza.fahmi99@gmail.com>
2020-05-01 16:20:24 +0700

    update show all post

--- script/mobile/k-allpost.user.js
+++ script/mobile/k-allpost.user.js
@@ -6,18 +6,20 @@
 // @icon          https://www.google.com/s2/favicons?domain=m.kaskus.co.id
 // @homepage      https://github.com/reforget-id/Simplified-Kaskus
 // @description   Otomatis redirect show all post
-// @include       https://m.kaskus.co.id/show_post/*
+// @include       https://*.kaskus.co.id/show_post/*
 // @downloadURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/mobile/k-allpost.user.js
 // @updateURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/mobile/k-allpost.user.js
+// @run-at        document-end
 // ==/UserScript==
 
 
-function showPost(){
-		var allpost = document.getElementsByClassName("Mend(15px)");
-		for (var i = 0; i < allpost.length ; i++) {
-				allpost[i].click();
+function showPost() {
+	var allpost = document.getElementsByClassName("Mend(15px)");
+	for (var i = 0; i < allpost.length; i++) {
+		if (allpost[i].className == "Mend(15px)") {
+			allpost[i].click();
 		}
-		
+	}
 }
 
 showPost();
--- script/pc/k-allpostpc.user.js
+++ script/pc/k-allpostpc.user.js
@@ -1,23 +1,25 @@
 // ==UserScript==
-// @name          Kaskus : Show All Post for PC
+// @name          Kaskus : Show All Post
 // @version       1.0.3
-// @namespace     k-allpostpc
+// @namespace     k-allpost
 // @author        ffsuperteam
 // @icon          https://www.google.com/s2/favicons?domain=m.kaskus.co.id
 // @homepage      https://github.com/reforget-id/Simplified-Kaskus
 // @description   Otomatis redirect show all post
-// @include       https://www.kaskus.co.id/show_post/*
-// @downloadURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/pc/k-allpost.user.js
-// @updateURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/pc/k-allpost.user.js
+// @include       https://*.kaskus.co.id/show_post/*
+// @downloadURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/mobile/k-allpost.user.js
+// @updateURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/mobile/k-allpost.user.js
+// @run-at        document-end
 // ==/UserScript==
 
 
-function showPost(){
+function showPost() {
     var allpost = document.getElementsByClassName("Mend(15px)");
-    for (var i = 0; i < allpost.length ; i++) {
+    for (var i = 0; i < allpost.length; i++) {
+        if (allpost[i].className == "Mend(15px)") {
             allpost[i].click();
+        }
     }
-    
 }
 
 showPost();
