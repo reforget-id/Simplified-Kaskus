3ca9b7566da8b98581dee2e4a3549b6638bdc127
reforget-id <fahriza.fahmi99@gmail.com>
2021-01-05 01:27:08 +0700

    update k-quote and k-allpost

--- script/pc/k-allpostpc.user.js
+++ script/pc/k-allpostpc.user.js
@@ -1,24 +1,36 @@
 // ==UserScript==
-// @name          Kaskus : Show All Post for PC
-// @version       2.0.2
+// @name          Kaskus : Show All Post for PC (Dev)
+// @version       2.1.0
 // @namespace     k-allpostpc
 // @author        ffsuperteam
 // @icon          https://s.kaskus.id/themes_3.0/mobile/images/logo-n.svg
 // @homepage      https://github.com/reforget-id/Simplified-Kaskus
-// @description   Otomatis redirect show all post
-// @match		  https://www.kaskus.co.id/show_post/*/-/*
-// @match		  https://www.kaskus.co.id/show_post/*/?*
-// @downloadURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/pc/k-allpostpc.user.js
-// @updateURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/pc/k-allpostpc.user.js
+// @description   Otomatis redirect single post ke all post (thread)
+// @match         https://www.kaskus.co.id/show_post/*/?child_id=*
+// @match         https://www.kaskus.co.id/show_post/*
+// @exclude       https://www.kaskus.co.id/show_post/*/*/-
+// @exclude       https://www.kaskus.co.id/show_post/*//-
+// @downloadURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/dev/script/pc/k-allpostpc.user.js
+// @updateURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/dev/script/pc/k-allpostpc.user.js
 // @run-at        document-start
 // ==/UserScript==
 
+'use strict';
 
-function redirect() {
-    var url = window.location.href;
-    var host = url.match(/^.*\.id\//g);
-    var post = url.match(/\w{24}/);
-    window.location.href = host + "post/" + post + "/#post" + post;
-}
+(() => {
+	const url = window.location.href
+    const parentId = url.match(/(?<=\/show_post\/)\w{24}/)
+    const childId = url.match(/(?<=child_id=)\w{24}/)
+    const postId = url.match(/#post\w{24}/)
 
-redirect();
+    if (childId) {
+        window.location.href = `https://www.kaskus.co.id/post/${parentId}/?child_id=post${childId}`
+    }
+    else if (!childId && postId) {
+        window.location.href = `https://www.kaskus.co.id/post/${parentId}/${postId}`
+    }
+    else {
+        return
+    }
+    
+})()
