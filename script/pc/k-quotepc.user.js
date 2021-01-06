6a31a63682644d3806d891cc2375d625748eed16
reforget-id <fahriza.fahmi99@gmail.com>
2021-01-06 23:04:24 +0700

    fix metadata indent

--- script/pc/k-quotepc.user.js
+++ script/pc/k-quotepc.user.js
@@ -1,23 +1,23 @@
 // ==UserScript==
-// @name		  	Kaskus : Insert Quote Button (PC)
-// @version			3.0.1
+// @name            Kaskus : Insert Quote Button (PC)
+// @version         3.0.1
 // @namespace		k-quotepc
-// @author			ffsuperteam
-// @icon			https://s.kaskus.id/themes_3.0/mobile/images/logo-n.svg
+// @author          ffsuperteam
+// @icon            https://s.kaskus.id/themes_3.0/mobile/images/logo-n.svg
 // @homepage		https://github.com/reforget-id/Simplified-Kaskus
 // @description		Tambah tombol quote di nested reply di PC
-// @match			https://www.kaskus.co.id/thread/*
-// @match			https://www.kaskus.co.id/post/*
-// @match			https://www.kaskus.co.id/lastpost/*
-// @include			https://www.kaskus.co.id/post_reply/*/
-// @downloadURL		https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/pc/k-quotepc.user.js
-// @updateURL		https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/pc/k-quotepc.user.js
-// @grant			GM_addStyle
-// @grant			GM_setValue
-// @grant			GM_getValue
-// @grant			GM_deleteValue
-// @grant			GM_xmlhttpRequest
-// @run-at			document-end
+// @match           https://www.kaskus.co.id/thread/*
+// @match           https://www.kaskus.co.id/post/*
+// @match           https://www.kaskus.co.id/lastpost/*
+// @include         https://www.kaskus.co.id/post_reply/*/
+// @downloadURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/pc/k-quotepc.user.js
+// @updateURL       https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/pc/k-quotepc.user.js
+// @grant           GM_addStyle
+// @grant           GM_setValue
+// @grant           GM_getValue
+// @grant           GM_deleteValue
+// @grant           GM_xmlhttpRequest
+// @run-at          document-end
 // ==/UserScript==
 
 
