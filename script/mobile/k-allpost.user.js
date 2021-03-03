0e38984879197ae59b0ade014e43cc619949d8c1
reforget-id <58993868+reforget-id@users.noreply.github.com>
2021-03-04 00:33:31 +0700

    Update k-allpost.user.js

--- script/mobile/k-allpost.user.js
+++ script/mobile/k-allpost.user.js
@@ -1,24 +1,33 @@
 // ==UserScript==
-// @name          Kaskus : Show All Post
-// @version       2.0.2
+// @name          Kaskus : Show All Post (Mobile Dev)
+// @version       2.1.0
 // @namespace     k-allpost
 // @author        ffsuperteam
 // @icon          https://s.kaskus.id/themes_3.0/mobile/images/logo-n.svg
 // @homepage      https://github.com/reforget-id/Simplified-Kaskus
-// @description   Otomatis redirect single post menjadi show all post
-// @match		  https://m.kaskus.co.id/show_post/*/-/*
-// @match		  https://m.kaskus.co.id/show_post/*/?*
+// @description   Otomatis redirect single post ke all post (thread)
+// @match         https://m.kaskus.co.id/show_post/*/?child_id=*
+// @match         https://m.kaskus.co.id/show_post/*
+// @exclude       https://m.kaskus.co.id/show_post/*/*/-
+// @exclude       https://m.kaskus.co.id/show_post/*//-
 // @downloadURL   https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/mobile/k-allpost.user.js
 // @updateURL     https://raw.githubusercontent.com/reforget-id/Simplified-Kaskus/master/script/mobile/k-allpost.user.js
 // @run-at        document-start
 // ==/UserScript==
 
+'use strict';
 
-function redirect() {
-	var url = window.location.href;
-	var host = url.match(/^.*\.id\//g);
-	var post = url.match(/\w{24}/);
-	window.location.href = host + "post/" + post + "/#post" + post;
-}
+(() => {
+	const url = window.location.href
+    const parentId = url.match(/(?<=\/show_post\/)\w{24}/)
+    const childId = url.match(/(?<=child_id=)\w{24}/)
+    const postId = url.match(/#post\w{24}/)
 
-redirect();
+    if (childId) {
+        window.location.href = `https://m.kaskus.co.id/post/${parentId}/?child_id=${childId}`
+    }
+    else if (postId) {
+        window.location.href = `https://m.kaskus.co.id/post/${parentId}${postId}`
+    }
+    
+})()
